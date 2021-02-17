bf01523cf9e74947eff0b59348f032bb0520ed6f
jmerges bootcamp <jamesmerges1@gmail.com>
2021-02-16 23:46:29 -0800

    Added skills

--- public/js/projects.js
+++ public/js/projects.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
         $("#link1").html(res[0].link);
         $("#link1").attr("href", res[0].link);
         $("#description1").html(res[0].description);
+        for (var i=0; i<res[0].Skills.length; i++) {
+            var $li = $("<li class='text-white'>");
+            $li.html(res[0].Skills[i].name);
+            $("#list1").append($li);
+        }
     });
 
     $.get("/api/projects/2", function(res) {
@@ -15,6 +20,11 @@ $(document).ready(function() {
         $("#link2").html(res[0].link);
         $("#link2").attr("href", res[0].link);
         $("#description2").html(res[0].description);
+        for (var i=0; i<res[0].Skills.length; i++) {
+            var $li = $("<li class='text-white'>");
+            $li.html(res[0].Skills[i].name);
+            $("#list2").append($li);
+        }
     });
 
     $.get("/api/projects/3", function(res) {
@@ -24,5 +34,10 @@ $(document).ready(function() {
         $("#link3").html(res[0].link);
         $("#link3").attr("href", res[0].link);
         $("#description3").html(res[0].description);
+        for (var i=0; i<res[0].Skills.length; i++) {
+            var $li = $("<li class='text-white'>");
+            $li.html(res[0].Skills[i].name);
+            $("#list3").append($li);
+        }
     });
 });
--- server.js
+++ server.js
@@ -21,18 +21,63 @@ db.Project.create({
   link: "https://github.com/michaelanthonyyy/aio-developer-homepage",
   name: "Programmer Home Page",
   description: "For this project, I worked with Michael Medina and Ryan Kirkland to create a coding homepage."
-});
+}).then(() => {
+  db.Skill.create({
+    name: "Leadership",
+    ProjectId: 1
+  });
+  
+  db.Skill.create({
+    name: "Group dynamics",
+    ProjectId: 1
+  });
+  
+  db.Skill.create({
+    name: "Page Design",
+    ProjectId: 1
+  });
+}).then(() => {
+  db.Project.create({
+    link: "https://github.com/jmerges/weather-dashboard",
+    name: "Weather Dashboard",
+    description: "This application searches for a city's weather data and displays it on the screen. It also forecasts the next 5 days for the city."
+  }).then(() => {
+    db.Skill.create({
+      name: "API requests",
+      ProjectId: 2
+    });
 
-db.Project.create({
-  link: "https://github.com/jmerges/weather-dashboard",
-  name: "Weather Dashboard",
-  description: "This application searches for a city's weather data and displays it on the screen. It also forecasts the next 5 days for the city."
-});
-
-db.Project.create({
-  link: "https://github.com/jmerges/County-Election-Results-Project",
-  name: "County Election Results",
-  description: "This project compares a searched county's presidential election results with its demographic statistics for the last 20 years."
+    db.Skill.create({
+      name: "JQuery",
+      ProjectId: 2
+    });
+  }).then(() => {
+    db.Project.create({
+      link: "https://github.com/jmerges/County-Election-Results-Project",
+      name: "County Election Results",
+      description: "This project compares a searched county's presidential election results with its demographic statistics for the last 20 years."
+    }).then(() => {
+      db.Skill.create({
+        name: "Chart JS",
+        ProjectId: 3
+      });
+      
+      db.Skill.create({
+        name: "Node",
+        ProjectId: 3
+      });
+      
+      db.Skill.create({
+        name: "Sequelize",
+        ProjectId: 3
+      });
+      
+      db.Skill.create({
+        name: "Advanced API requests",
+        ProjectId: 3
+      });
+    });
+  });
 });
   });
 });
